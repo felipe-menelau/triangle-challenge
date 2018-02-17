0e9b49f028825a3bcdedeae00f6ae1837f925ead
Felipe Menelau <felipe.menelau@inlocomedia.com>
2018-02-17 09:45:06 -0300

    Fixes some poor identation

--- challenge/main.py
+++ challenge/main.py
@@ -4,10 +4,10 @@ def solve(triangle = []):
 
     bottom_up_navigation_range = reversed(range(len(triangle) - 1))
 
-	for i in bottom_up_navigation_range:
-		for j in range(len(triangle[i])):
-			triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1])
-	return str(triangle[0][0])
+    for i in bottom_up_navigation_range:
+        for j in range(len(triangle[i])):
+            triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1])
+    return str(triangle[0][0])
 
 
 
